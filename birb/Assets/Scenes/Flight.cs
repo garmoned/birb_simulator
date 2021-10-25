666421e045c805416129fa7a2e9bf3769a332740
thuavu <thuvu2100@gmail.com>
2021-10-25 17:06:52 -0500

    Player Controls via Arrow Keys

--- birb/Assets/Scenes/Flight.cs
+++ birb/Assets/Scenes/Flight.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Flight : MonoBehaviour
 {
+
+    float velocity = 0.0f;
+    float accel = -0.1f;
+
+    public GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        camera.transform.Translate(new Vector3(0, velocity, 0));
+        velocity += accel;
     }
 }
